9c9c6df7452f8fa1866622856c117a734898d84b
matej13 <matej.ristic13@gmail.com>
2018-02-15 00:43:09 +0100

    change 2 wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,8 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "Hello World!123"
+#def hello():
+#    return "Hello World!123"
 def test(): #program does nothing as written
     return "Test to see if it works"
 
